a2a731add8dbe1c2e409ec40b2d12826b3903720
patrick1928 <81092778+patrick1928@users.noreply.github.com>
2023-05-29 22:05:53 -0300

    Update cadUsers.php

--- php/cadUsers.php
+++ php/cadUsers.php
@@ -8,20 +8,19 @@ $nascimento = $_POST['cadData-nascimento'];
 $infoRequest = "INSERT IGNORE INTO users(id, nome, email, senha, nascimento)
 VALUES('', '$nome','$email','$senha','$nascimento')";
 $verif = "select email from users";
-$a = mysqli_query($conexao, $verif);
 mysqli_query($conexao, $infoRequest);
-if($a == $email){
+
     echo "<script>
 alert('Endereço de E-mail já cadastrado em nosso sistema');
-window.location = 'accont.html';
+window.location = '/github/loja-online/accont.html';
 
 </script>";
-}else{
+
 echo "<script>
 alert('usuario cadastrado com sucesso!');
 window.location = '/github/loja-online/accont.html';
 
 </script>";
-}
+
 mysqli_close($conexao);
 ?>

722c81822ff1362c10468c4bdad3719bbc44b1d9
patrick1928 <81092778+patrick1928@users.noreply.github.com>
2023-05-31 10:20:24 -0300

    d

--- script/index.js
+++ script/index.js
@@ -13,34 +13,3 @@ menubtn.addEventListener('click', () => {
         menubtn.style.left = '0%';
     }
 })
-function verifUser(x){
-    const inputsCad = [document.getElementById('idname'), document.getElementById('idemail'), document.getElementById('idpassword'), document.getElementById('idnasc')]
-    if(x == 1){
-        for(let x in inputsCad){
-            if(!inputsCad[x].value == ''){
-                inputsCad[x].style.border = '1px solid green'
-                
-             }else{
-                inputsCad[x].style.border = '1px solid red'
-                alert(`preencha o seguinte campo: ${inputsCad[x].name}!`)
-                return
-             }
-        }
-        document.getElementById('cad').type = 'submit';
-        document.getElementById('cadUser').action = 'php/cadUsers.php';
-        
-    }else{
-        for(let x in inputsCad){
-            if(!inputsCad[x].value == ''){
-                inputsCad[x].style.border = '1px solid green'
-                
-             }else{
-                inputsCad[x].style.border = '1px solid red'
-                alert(`preencha o seguinte campo: ${inputsCad[x].name}!`)
-                return
-             }
-        }
-        document.getElementById('cad').type = 'submit';
-        document.getElementById('cadUser').action = 'php/cadUsers.php';
-    }
-}

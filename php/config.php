4934cb6f9fa377458542ccf21c2c1c623466eddb
patrick1928 <81092778+patrick1928@users.noreply.github.com>
2023-06-02 15:23:58 -0300

    mudando o backend para nodejs

--- php/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host = "localhost";
-$name = "dbpatrick";
-$password = "";
-$user = "root";
-$sql = mysqli_init();
-$conexao = mysqli_connect($host, $user, $password,$name);
-
-?>
--- php/verif.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('config.php');
-$sql = "SELECT * FROM users";
-$verif = mysqli_query($conexao,$sql);
-while($userData = mysqli_fetch_assoc($verif)){
-    echo "$userData[name]";
-}
-?>

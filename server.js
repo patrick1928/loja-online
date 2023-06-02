f05b64430013882c3431ec62c1266d0fab623959
patrick1928 <81092778+patrick1928@users.noreply.github.com>
2023-06-02 15:24:19 -0300

    mudando o back para node

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const app = express();
 
-const phpExpress = require('php-express');
-const phpExpressEngine = phpExpress({
-    binPath: 'php'
-});
-
 app.use(express.static('public'));
-app.set('views', './views')
-app.engine('php', phpExpressEngine);
-app.set('view engine', 'php');
 
-app.all(/\.php$/, phpExpressEngine);
 
+//arquivos html!!
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 app.get('/accont', (req, res) => {
     res.sendFile(__dirname + '/accont.html');
+
+
+//arquivos css!!
 });
 app.get('/css/style', (req, res) => {
     res.sendFile(__dirname + '/css/style.css');
+
+    //imagens
 });
 app.get('/imagens', (req, res) => {
     res.sendFile(__dirname + '/imagens/tecladoGamer.jpg')
 })
+
+//arquivos js
+app.get('/script/index', (req, res) => {
+    res.sendFile(__dirname + '/script/index.js')
+})
+
+//rodar o servidor
 app.listen(3000, () => {
     console.log('servidor web iniciado na porta 3000!')
 });

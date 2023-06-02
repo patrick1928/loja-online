83a1a1e57da3f0b2da878022d14670e1bc185306
patrick1928 <81092778+patrick1928@users.noreply.github.com>
2023-06-02 12:54:11 -0300

    asd

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
+const phpExpress = require('php-express');
+const phpExpressEngine = phpExpress({
+    binPath: 'php'
+});
+
 app.use(express.static('public'));
+app.set('views', './views')
+app.engine('php', phpExpressEngine);
+app.set('view engine', 'php');
 
-
+app.all(/\.php$/, phpExpressEngine);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -14,6 +22,9 @@ app.get('/accont', (req, res) => {
 app.get('/css/style', (req, res) => {
     res.sendFile(__dirname + '/css/style.css');
 });
+app.get('/imagens', (req, res) => {
+    res.sendFile(__dirname + '/imagens/tecladoGamer.jpg')
+})
 app.listen(3000, () => {
     console.log('servidor web iniciado na porta 3000!')
 });

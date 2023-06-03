3919385b58de3d52e1439bdd0805a42e4ebba681
patrick1928 <81092778+patrick1928@users.noreply.github.com>
2023-06-03 17:16:27 -0300

    to entendendo e nada

---
+++ main.js
@@ -0,0 +1,29 @@
+const Express = require('express');
+const app = new Express();
+const PORT = 3000;
+//caminhos
+//arquivos html
+app.get('/' , (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/index.html' , (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+app.get('/accont.html', (req, res)=> {
+    res.sendFile(__dirname + '/accont.html')
+})
+
+//arquivos css
+app.get('/css/style.css', (req, res)=> {
+    res.sendFile(__dirname + '/css/style.css')
+
+})
+
+// arquivos js
+app.get('/src/index.js' , (req, res) => {
+    res.sendFile(__dirname + '/src/index.js')
+})
+
+app.listen(PORT , (req, res) => {
+    console.log(`servidor rodando no ${PORT}`);
+})
--- script/queryUser.js
+++
@@ -1,5 +0,0 @@
-const app = require('../server');
-app.post('/script/queryUser',(req, res) => {
-    const{ email, senha} = req.body;
-    res.send(email)
-})
